4ea1babe41268718a62c79a0edd10ac7c2d13d59
jeehoonkim-youha <jeehoon.kim@youha.info>
2022-06-23 11:53:30 +0900

    add lambda handler with required packages

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+export async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    return app;
 }
-bootstrap();
+
+async function run() {
+    const nestApp = await bootstrap();
+    await nestApp.listen(3000);
+}
+
+run();
